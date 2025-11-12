a461bf8a12c7c1296e26c9b3da55dfaf9a64e870
NeKsiKZ <bz12330@gmail.com>
2025-11-13 00:35:43 +0100

    feat: Deploy to Azure and fix session persistence with Blob Storage

--- VendingManager/Program.cs
+++ VendingManager/Program.cs
@@ -5,92 +5,103 @@ using Microsoft.AspNetCore.Identity;
 using VendingManager.Filters;
 using VendingManager.Models;
 using VendingManager.Hubs;
+using Microsoft.AspNetCore.DataProtection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+string? storageConnectionString = builder.Configuration.GetConnectionString("DataProtectionStorage");
+
+if (!string.IsNullOrEmpty(storageConnectionString))
+{
+    builder.Services.AddDataProtection()
+        .PersistKeysToAzureBlobStorage(storageConnectionString,
+                                       "data-protection-keys",
+                                       "keys.xml");
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
         sqlServerOptions => sqlServerOptions.EnableRetryOnFailure()
     ));
 
-builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = false;
-})
-.AddEntityFrameworkStores<ApplicationDbContext>()
-.AddDefaultUI()
-.AddDefaultTokenProviders();
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddSignalR();
-
-builder.Services.AddScoped<ApiKeyAuthFilter>();
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
+    builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
     {
-        var dbContext = services.GetRequiredService<ApplicationDbContext>();
-        dbContext.Database.Migrate();
-    }
-    catch (Exception ex)
+        options.SignIn.RequireConfirmedAccount = false;
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    builder.Services.AddSignalR();
+
+    builder.Services.AddScoped<ApiKeyAuthFilter>();
+
+    var app = builder.Build();
+
+    using (var scope = app.Services.CreateScope())
     {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "Wystąpił błąd podczas automatycznej migracji bazy danych.");
+        var services = scope.ServiceProvider;
+        try
+        {
+            var dbContext = services.GetRequiredService<ApplicationDbContext>();
+            dbContext.Database.Migrate();
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "Wystąpił błąd podczas automatycznej migracji bazy danych.");
+        }
     }
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            await IdentityDataSeeder.SeedRolesAndAdminAsync(services);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "Wystąpił błąd podczas seedowania bazy danych Identity.");
+        }
+    }
+
+    var supportedCultures = new[] { new CultureInfo("pl-PL") };
+    var localizationOptions = new RequestLocalizationOptions
+    {
+        DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("pl-PL"),
+        SupportedCultures = supportedCultures,
+        SupportedUICultures = supportedCultures
+    };
+    app.UseRequestLocalization(localizationOptions);
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Dashboard/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    //app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
+    app.MapRazorPages();
+
+    app.MapHub<DashboardHub>("/dashboardHub");
+
+    app.Run();
 }
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        await IdentityDataSeeder.SeedRolesAndAdminAsync(services);
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "Wystąpił błąd podczas seedowania bazy danych Identity.");
-    }
-}
-
-var supportedCultures = new[] { new CultureInfo("pl-PL") };
-var localizationOptions = new RequestLocalizationOptions
-{
-    DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("pl-PL"),
-    SupportedCultures = supportedCultures,
-    SupportedUICultures = supportedCultures
-};
-app.UseRequestLocalization(localizationOptions);
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Dashboard/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-//app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
-
-app.MapRazorPages();
-
-app.MapHub<DashboardHub>("/dashboardHub");
-
-app.Run();
