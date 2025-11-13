bdf2f13d970c0382da5822029f7e44a8423b3551
NeKsiKZ <bz12330@gmail.com>
2025-11-13 01:09:45 +0100

    Repair program.cs }

--- VendingManager/Program.cs
+++ VendingManager/Program.cs
@@ -17,91 +17,94 @@ if (!string.IsNullOrEmpty(storageConnectionString))
         .PersistKeysToAzureBlobStorage(storageConnectionString,
                                        "data-protection-keys",
                                        "keys.xml");
+}
 
-    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
         sqlServerOptions => sqlServerOptions.EnableRetryOnFailure()
     ));
 
-    builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultUI()
+.AddDefaultTokenProviders();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSignalR();
+
+builder.Services.AddScoped<ApiKeyAuthFilter>();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
     {
-        options.SignIn.RequireConfirmedAccount = false;
-    })
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultUI()
-    .AddDefaultTokenProviders();
-
-    // Add services to the container.
-    builder.Services.AddControllersWithViews();
-
-    builder.Services.AddSignalR();
-
-    builder.Services.AddScoped<ApiKeyAuthFilter>();
-
-    var app = builder.Build();
-
-    using (var scope = app.Services.CreateScope())
-    {
-        var services = scope.ServiceProvider;
-        try
-        {
-            var dbContext = services.GetRequiredService<ApplicationDbContext>();
-            dbContext.Database.Migrate();
-        }
-        catch (Exception ex)
-        {
-            var logger = services.GetRequiredService<ILogger<Program>>();
-            logger.LogError(ex, "Wystąpił błąd podczas automatycznej migracji bazy danych.");
-        }
+        var dbContext = services.GetRequiredService<ApplicationDbContext>();
+        dbContext.Database.Migrate();
     }
-
-    using (var scope = app.Services.CreateScope())
+    catch (Exception ex)
     {
-        var services = scope.ServiceProvider;
-        try
-        {
-            await IdentityDataSeeder.SeedRolesAndAdminAsync(services);
-        }
-        catch (Exception ex)
-        {
-            var logger = services.GetRequiredService<ILogger<Program>>();
-            logger.LogError(ex, "Wystąpił błąd podczas seedowania bazy danych Identity.");
-        }
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Wystąpił błąd podczas automatycznej migracji bazy danych.");
     }
-
-    var supportedCultures = new[] { new CultureInfo("pl-PL") };
-    var localizationOptions = new RequestLocalizationOptions
-    {
-        DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("pl-PL"),
-        SupportedCultures = supportedCultures,
-        SupportedUICultures = supportedCultures
-    };
-    app.UseRequestLocalization(localizationOptions);
-
-    // Configure the HTTP request pipeline.
-    if (!app.Environment.IsDevelopment())
-    {
-        app.UseExceptionHandler("/Dashboard/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
-
-    //app.UseHttpsRedirection();
-    app.UseStaticFiles();
-
-    app.UseRouting();
-
-    app.UseAuthentication();
-
-    app.UseAuthorization();
-
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Dashboard}/{action=Index}/{id?}");
-
-    app.MapRazorPages();
-
-    app.MapHub<DashboardHub>("/dashboardHub");
-
-    app.Run();
 }
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        await IdentityDataSeeder.SeedRolesAndAdminAsync(services);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Wystąpił błąd podczas seedowania bazy danych Identity.");
+    }
+}
+
+var supportedCultures = new[] { new CultureInfo("pl-PL") };
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("pl-PL"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+};
+app.UseRequestLocalization(localizationOptions);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Dashboard/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") != "true")
+{
+    app.UseHttpsRedirection();
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.MapHub<DashboardHub>("/dashboardHub");
+
+app.Run();
